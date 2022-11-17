5293f8a75477b89e6c56602098503872fb9a9187
Faithful <faithfulolaleru09@gmail.com>
2022-11-17 17:30:07 +0100

    add index route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,13 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import userRouter from './userRoute';
 import walletRouter from './walletRoute';
 import transactionRouter from './transactionRoute';
 
 const router = Router();
 
+router.use('/', (req: Request, res: Response) => {
+    res.send("Deployed Successfully"); 
+});
 router.use('/users', userRouter);
 router.use('/wallets', walletRouter);
 router.use('/transactions', transactionRouter);
