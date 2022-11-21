427f62a67dc0b09a1ecfc62f9729d2b2fa80b5f9
Faithful <faithfulolaleru09@gmail.com>
2022-11-21 11:06:08 +0100

    skip pool.getConnection, call query directly

--- src/handlers/transactionHandler.ts
+++ src/handlers/transactionHandler.ts
@@ -19,190 +19,169 @@ const createTransfer = async (req: Request, res: Response) => {
         receiverBankAccount 
     } = req.body;
 
-    pool.getConnection((err: any, conn: any) => {
+    // make sure sender wallet exist
+
+    // const sqlQuery = 'SELECT * FROM wallets WHERE wallet_id IN (?, ?)';
+    const sqlQuery = 'SELECT * FROM wallets WHERE wallet_id=?';
+
+    pool.query(sqlQuery, [senderWalletId], async (err: any, rows: any) => {
         if(err){
-            console.log('Entered an error: ', err);
+            console.log('Encountered an error: ', err);
+    
+            return res.send({
+                success: false,
+                statusCode: 400
+            });      
+        }
+
+        if(rows.length < 1){   
+
+            return res.send({
+                message: 'Sender wallet not found',
+                statusCode: 404,
+            });
+        }
+
+        // make sure logged in user is the owner of sender wallet
+        if(rows[0].user_id !== currentUserId){   
+
+            return res.send({
+                message: 'Logged in User cannot transact with this wallet',
+                statusCode: 403,
+            });
+        }
+
+        // make sure receiver exists 
+        let receiverWalletDetails: any;
+
+        try {
+            receiverWalletDetails = await getWalletByWalletId(receiverWalletId);
+        } catch (error) {
+            console.log('Entered an error --> ', error);
             res.send({
                 success: false,
                 statusCode: 500,
-                message: 'Error during connection'
+                message: 'Error retrieving receiver wallet details'
             }) 
             
             return;
-        } 
-  
-        // make sure sender wallet exist
-
-        // const sqlQuery = 'SELECT * FROM wallets WHERE wallet_id IN (?, ?)';
-        const sqlQuery = 'SELECT * FROM wallets WHERE wallet_id=?';
-
-        pool.query(sqlQuery, [senderWalletId], async (err: any, rows: any) => {
-            if(err){
-                console.log('Encountered an error: ', err);
-                conn.release();
+        }
+            
+        console.log("receiverWalletDetailsWalletId in transactionHandler --> " + receiverWalletDetails.wallet_id);
+        console.log("receiverWalletDetailsUserId in transactionHandler --> " + receiverWalletDetails.user_id);
+        console.log("receiverWalletDetailsBalance in transactionHandler --> " + receiverWalletDetails.balance);
         
-                return res.send({
-                    success: false,
-                    statusCode: 400
-                });      
-            }
-    
-            if(rows.length < 1){   
-                conn.release();
+        const { wallet_id, user_id, balance } = receiverWalletDetails;
+
+        // no need to check if bank account exists, 3rd party integration would handle that
+
+        if(wallet_id !== null){   
+            // check if wallet balance is sufficient 
+            
+            const senderCurrentBalance = rows[0].balance;
+
+            if(senderCurrentBalance < transactionAmount){
 
                 return res.send({
-                    message: 'Sender wallet not found',
-                    statusCode: 404,
-                });
-            }
-
-            // make sure logged in user is the owner of sender wallet
-            if(rows[0].user_id !== currentUserId){   
-                conn.release();
-
-                return res.send({
-                    message: 'Logged in User cannot transact with this wallet',
+                    message: 'You do not have sufficient balance to make this transaction',
                     statusCode: 403,
                 });
             }
 
-            // make sure receiver exists 
-            let receiverWalletDetails: any;
+            // create transaction
+            const sqlQuery2 = `INSERT INTO transactions(transaction_id, transaction_type, transaction_amount, 
+                user_id, sender_wallet_id, receiver_wallet_id, receiver_bank_account) VALUES (?,?,?,?,?,?,?)`;
 
-            try {
-                receiverWalletDetails = await getWalletByWalletId(receiverWalletId);
-            } catch (error) {
-                console.log('Entered an error --> ', error);
-                res.send({
-                    success: false,
-                    statusCode: 500,
-                    message: 'Error retrieving receiver wallet details'
-                }) 
-                
-                return;
-            }
-             
-            console.log("receiverWalletDetailsWalletId in transactionHandler --> " + receiverWalletDetails.wallet_id);
-            console.log("receiverWalletDetailsUserId in transactionHandler --> " + receiverWalletDetails.user_id);
-            console.log("receiverWalletDetailsBalance in transactionHandler --> " + receiverWalletDetails.balance);
+            pool.query(sqlQuery2, [uuid(), transactionType, transactionAmount, currentUserId, senderWalletId, 
+                receiverWalletId, receiverBankAccount], async (err: any, rows: any) => { 
+
+                if(err){
+                    console.log('Encountered an error: ', err);
             
-            const { wallet_id, user_id, balance } = receiverWalletDetails;
-
-            // no need to check if bank account exists, 3rd party integration would handle that
-
-            if(wallet_id !== null){   
-                // check if wallet balance is sufficient 
-                
-                const senderCurrentBalance = rows[0].balance;
-
-                if(senderCurrentBalance < transactionAmount){
-                    conn.release();
-
                     return res.send({
-                        message: 'You do not have sufficient balance to make this transaction',
-                        statusCode: 403,
-                    });
+                        success: false,
+                        statusCode: 400
+                    });      
                 }
 
-                // create transaction
-                const sqlQuery2 = `INSERT INTO transactions(transaction_id, transaction_type, transaction_amount, 
-                    user_id, sender_wallet_id, receiver_wallet_id, receiver_bank_account) VALUES (?,?,?,?,?,?,?)`;
+                // update sender wallet balance
 
-                pool.query(sqlQuery2, [uuid(), transactionType, transactionAmount, currentUserId, senderWalletId, 
-                    receiverWalletId, receiverBankAccount], async (err: any, rows: any) => { 
+                let updateSenderWalletBalanceResult: any;
 
-                    if(err){
-                        console.log('Encountered an error: ', err);
-                        conn.release();
-                
-                        return res.send({
-                            success: false,
-                            statusCode: 400
-                        });      
-                    }
+                try {
+                    updateSenderWalletBalanceResult = await updateWalletBalance(senderCurrentBalance, transactionAmount, senderWalletId, "sender");
+                    console.log('updateSenderWalletBalanceResult in transaction handler --> ', updateSenderWalletBalanceResult);
 
-                    // update sender wallet balance
-
-                    let updateSenderWalletBalanceResult: any;
-
-                    try {
-                        updateSenderWalletBalanceResult = await updateWalletBalance(senderCurrentBalance, transactionAmount, senderWalletId, "sender");
-                        console.log('updateSenderWalletBalanceResult in transaction handler --> ', updateSenderWalletBalanceResult);
-
-                        if(updateSenderWalletBalanceResult !== 1){
-                            conn.release();
-                            console.log('Error from updating Sender wallet balance');
-                            res.send({
-                                success: false,
-                                statusCode: 400,
-                                message: 'Error from updating Sender wallet balance'
-                            }) 
-                            
-                            return;
-                        }
-                    } catch (error) {
-                        console.log('Entered an error --> ', error);
+                    if(updateSenderWalletBalanceResult !== 1){
+                        console.log('Error from updating Sender wallet balance');
                         res.send({
                             success: false,
-                            statusCode: 500,
-                            message: 'Error retrieving update wallet balance status for sender'
+                            statusCode: 400,
+                            message: 'Error from updating Sender wallet balance'
                         }) 
                         
                         return;
                     }
-
-                    
-                    // update receiver balance
-
-                    let updateReceiverWalletBalanceResult: any;
-
-                    try {
-                        updateReceiverWalletBalanceResult = await updateWalletBalance(balance, transactionAmount, receiverWalletId, "receiver");
-                        console.log('updateReceiverWalletBalanceResult in transaction handler --> ', updateReceiverWalletBalanceResult);
-
-                        if(updateReceiverWalletBalanceResult !== 1){
-                            conn.release();
-                            console.log('Error from updating Receiver wallet balance');
-                            res.send({
-                                success: false,
-                                statusCode: 400,
-                                message: 'Error from updating Receiver wallet balance'
-                            }) 
-                            
-                            return;
-                        }
-                    } catch (error) {
-                        console.log('Entered an error --> ', error);
-                        res.send({
-                            success: false,
-                            statusCode: 500,
-                            message: 'Error retrieving update wallet balance status for receiver'
-                        }) 
-                        
-                        return;
-                    }
-
-
+                } catch (error) {
+                    console.log('Entered an error --> ', error);
                     res.send({
-                        message: 'Transaction successful',
-                        statusCode: 200,
-                        // data: rows
-                    });
-            
-                    conn.release();   // close connection
-                });
-            } else {
-                conn.release();
+                        success: false,
+                        statusCode: 500,
+                        message: 'Error retrieving update wallet balance status for sender'
+                    }) 
+                    
+                    return;
+                }
 
-                return res.send({
-                    message: 'Receiver wallet does not exist',
-                    statusCode: 404,
+                
+                // update receiver balance
+
+                let updateReceiverWalletBalanceResult: any;
+
+                try {
+                    updateReceiverWalletBalanceResult = await updateWalletBalance(balance, transactionAmount, receiverWalletId, "receiver");
+                    console.log('updateReceiverWalletBalanceResult in transaction handler --> ', updateReceiverWalletBalanceResult);
+
+                    if(updateReceiverWalletBalanceResult !== 1){
+                        console.log('Error from updating Receiver wallet balance');
+                        res.send({
+                            success: false,
+                            statusCode: 400,
+                            message: 'Error from updating Receiver wallet balance'
+                        }) 
+                        
+                        return;
+                    }
+                } catch (error) {
+                    console.log('Entered an error --> ', error);
+                    res.send({
+                        success: false,
+                        statusCode: 500,
+                        message: 'Error retrieving update wallet balance status for receiver'
+                    }) 
+                    
+                    return;
+                }
+
+
+                res.send({
+                    message: 'Transaction successful',
+                    statusCode: 200,
+                    // data: rows
                 });
-            }
-            
-        });
-      
+        
+                // pool.query closes connection by itself  
+            });
+        } else {
+
+            return res.send({
+                message: 'Receiver wallet does not exist',
+                statusCode: 404,
+            });
+        }
+        
     });
+
 }
 
 export default createTransfer;
--- src/handlers/walletHandler.ts
+++ src/handlers/walletHandler.ts
@@ -7,94 +7,76 @@ const createWallet = (req: Request, res: Response) => {
     // console.log("(<any>req).user from walletRoute: " + (<any>req).user);
     const currentUserEmail =  (<any>req).user;
 
-    pool.getConnection((err: any, conn: any) => {
+    // console.log('isbn from req.body: ' + req.body.isbn);
+  
+    pool.query('SELECT * FROM users WHERE email=?', [currentUserEmail], (err: any, rows: any) => {
         if(err){
-            console.log('Entered an error: ', err);
-            res.send({
+            console.log('Encountered an error: ', err)
+    
+            return res.send({
                 success: false,
-                statusCode: 500,
-                message: 'Error during connection'
-            }) 
-            
-            return;
+                statusCode: 400
+            });      
         }
 
-        // console.log('isbn from req.body: ' + req.body.isbn);
-  
-        pool.query('SELECT * FROM users WHERE email=?', [currentUserEmail], (err: any, rows: any) => {
-            if(err){
-                console.log('Encountered an error: ', err);
-                conn.release();
-        
-                return res.send({
-                    success: false,
-                    statusCode: 400
-                });      
-            }
-    
-            if(rows.length < 1){   
-                conn.release();
+        if(rows.length < 1){  
 
-                return res.send({
-                    message: 'User not found',
-                    statusCode: 404,
-                });
-            } else {
-                // User exists, create wallet for user
-                const userId = rows[0].user_id;
-                const searchQuery = 'SELECT * FROM wallets WHERE user_id=?';
+            return res.send({
+                message: 'User not found',
+                statusCode: 404,
+            });
+        } else {
+            // User exists, create wallet for user
+            const userId = rows[0].user_id;
+            const searchQuery = 'SELECT * FROM wallets WHERE user_id=?';
 
-                pool.query(searchQuery, [userId], (err: any, rows: any) => {
+            pool.query(searchQuery, [userId], (err: any, rows: any) => {
+                if(err){
+                    console.log('Encountered an error: ', err);
+            
+                    return res.send({
+                        success: false,
+                        statusCode: 400
+                    });      
+                }
+
+                // add unique constraint to DB for user_id
+                if(rows.length >= 1){   
+
+                    return res.send({
+                        message: 'User already has a wallet',
+                        statusCode: 409,
+                    });
+                }
+
+                const sqlQuery = `INSERT INTO wallets(wallet_id, user_id, balance) VALUES (?,?,?)`;
+                // const { walletId, userId, balance } = req.body;
+
+                pool.query(sqlQuery, [uuid(), userId, 0.0], (err: any, rows: any) => {
                     if(err){
                         console.log('Encountered an error: ', err);
-                        conn.release();
                 
                         return res.send({
                             success: false,
                             statusCode: 400
                         });      
                     }
-
-                    // add unique constraint to DB for user_id
-                    if(rows.length >= 1){   
-                        conn.release();
-        
-                        return res.send({
-                            message: 'User already has a wallet',
-                            statusCode: 409,
-                        });
-                    }
-
-                    const sqlQuery = `INSERT INTO wallets(wallet_id, user_id, balance) VALUES (?,?,?)`;
-                    // const { walletId, userId, balance } = req.body;
-
-                    pool.query(sqlQuery, [uuid(), userId, 0.0], (err: any, rows: any) => {
-                        if(err){
-                            console.log('Encountered an error: ', err);
-                            conn.release();
-                    
-                            return res.send({
-                                success: false,
-                                statusCode: 400
-                            });      
-                        }
-                
-                        res.send({
-                            message: 'Successul',
-                            statusCode: 200,
-                            // data: rows
-                        });
-                
-                        conn.release();   // close connection
+            
+                    res.send({
+                        message: 'Successul',
+                        statusCode: 200,
+                        // data: rows
                     });
-
+            
+                    // pool.query closes connection by itself  
                 });
 
-            }
-    
-        });
-      
+            });
+
+        }
+
     });
+      
 }
 
 export default createWallet;
